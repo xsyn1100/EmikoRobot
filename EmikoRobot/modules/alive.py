9031a7b0a8263d39d351ee6c55d7e28e3a5785cc
xsyn1100 <98083245+xsyn1100@users.noreply.github.com>
2022-02-09 19:18:43 +0700

    Update alive.py

--- EmikoRobot/modules/alive.py
+++ EmikoRobot/modules/alive.py
@@ -9,7 +9,6 @@ from EmikoRobot.events import register
 from EmikoRobot import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/2c13e570944404a6c0d86.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
