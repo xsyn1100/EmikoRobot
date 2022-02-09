5dde4c88d869a03ecd47ee79ed4ac7f024884654
xsyn1100 <98083245+xsyn1100@users.noreply.github.com>
2022-02-09 22:56:39 +0700

    Update tagall.py

--- EmikoRobot/modules/tagall.py
+++ EmikoRobot/modules/tagall.py
@@ -15,32 +15,23 @@ spam_chats = []
 async def mentionall(event):
     chat_id = event.chat_id
     if event.is_private:
-        return await event.respond("__This command can be use in groups and channels!__")
+        return await event.respond("This command can be use in groups and channels!")
 
     is_admin = False
     try:
-        partici_ = await client(GetParticipantRequest(
-            event.chat_id,
-            event.sender_id
-        ))
+        partici_ = await Client(GetParticipantRequest(event.chat_id, event.sender_id))
     except UserNotParticipantError:
         is_admin = False
     else:
-        if (
-                isinstance(
-                    partici_.participant,
-                    (
-                            ChannelParticipantAdmin,
-                            ChannelParticipantCreator
-                    )
-                )
+        if isinstance(
+            partici_.participant, (ChannelParticipantAdmin, ChannelParticipantCreator)
         ):
             is_admin = True
     if not is_admin:
-        return await event.reply("__Only admins can mention all!__")
+        return await event.respond("Only admins can mention all!")
 
     if event.pattern_match.group(1) and event.is_reply:
-        return await event.reply("__Give me one argument!__")
+        return await event.respond("Give me one argument!")
     elif event.pattern_match.group(1):
         mode = "text_on_cmd"
         msg = event.pattern_match.group(1)
@@ -49,27 +40,30 @@ async def mentionall(event):
         msg = await event.get_reply_message()
         if msg == None:
             return await event.respond(
-                "__I can't mention members for older messages! (messages which are sent before I'm added to group)__")
+                "I can't mention members for older messages! (messages which are sent before I'm added to group)"
+            )
     else:
-        return await event.reply("__Reply to a message or give me some text to mention others!__")
+        return await event.respond(
+            "Reply to a message or give me some text to mention others!"
+        )
 
-    spam_chats.append(chat_id)
+    Spam = spam_chats.append(chat_id)
     usrnum = 0
-    usrtxt = ''
-    async for usr in client.iter_participants(chat_id):
+    usrtxt = ""
+    async for usr in Client.iter_participants(chat_id):
         if not chat_id in spam_chats:
             break
         usrnum += 1
-        usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}), "
+        usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
         if usrnum == 5:
             if mode == "text_on_cmd":
-                txt = f"{msg}\n{usrtxt}"
-                await client.send_message(chat_id, txt)
+                txt = f"{usrtxt}\n\n{msg}"
+                await Client.send_message(chat_id, txt)
             elif mode == "text_on_reply":
                 await msg.reply(usrtxt)
             await asyncio.sleep(2)
             usrnum = 0
-            usrtxt = ''
+            usrtxt = ""
     try:
         spam_chats.remove(chat_id)
     except:
