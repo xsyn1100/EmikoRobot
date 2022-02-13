d560650f97a27054d5eb76791743f12d3414457a
xsyn1100 <98083245+xsyn1100@users.noreply.github.com>
2022-02-13 15:45:30 +0700

    Update bug.py

--- EmikoRobot/modules/bug.py
+++ EmikoRobot/modules/bug.py
@@ -45,7 +45,7 @@ async def bug(_, msg: Message):
     datetimes_fmt = "%d-%m-%Y"
     datetimes = datetime.utcnow().strftime(datetimes_fmt)
 
-    thumb = "https://telegra.ph/file/51894f2116cf09a0fba15.jpg".
+    thumb = "https://telegra.ph/file/51894f2116cf09a0fba15.jpg"
     
     bug_report = f"""
 **#BUG : ** **@{owner_usn}**
